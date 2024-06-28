da621ca6bd41803184fdd2b7e760a2133a6e41b5
Jeanett Melsom <114770676+JeanettKM@users.noreply.github.com>
2024-06-28 09:28:22 +0200

    Changed to use the implicit return

--- Frontend-Dev Eksamen 2/src/components/RegisterPage/RegisterPage.jsx
+++ Frontend-Dev Eksamen 2/src/components/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import RegisterForm from "./RegisterForm";
 
-const RegisterPage = () => {
-  return (
-    <div>
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container">
-          <h1 className="display-4">Welcome to Holidaze</h1>
-          <p className="lead">
-            Register a account to check out our selection of hotels and venues.
-          </p>
-          <RegisterForm />
-        </div>
+const RegisterPage = () => (
+  <div>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Welcome to Holidaze</h1>
+        <p className="lead">
+          Register a account to check out our selection of hotels and venues.
+        </p>
+        <RegisterForm />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default RegisterPage;
